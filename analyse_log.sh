128065acb2eb829c1c7bfb206afa424eacb9ed64
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-04-28 16:27:05 -0300

    adapted to work for radix

--- analyse_log.sh
+++ analyse_log.sh
@@ -29,10 +29,10 @@ highest=${sorted[-1]}
 
 # print the lines with the lowest, highest, and average number of operations
 printf "$GREEN< best case arrays >\n$DEF_COLOR"
-grep "number of operations: $lowest$" $log_file
+cat $log_file | grep "number of operations: $lowest$" | head -n 2
 
 printf "$GREEN<worst case arrays >\n$DEF_COLOR"
-grep "number of operations: $highest$" $log_file
+cat $log_file | grep "number of operations: $highest$" | head -n 2
 
 # print the lowest, highest, and average number of operations
 printf "\n$HGREEN							< MOST IMPORTANT FOR THE EVALUATION > $CYAN average case: $average$DEF_COLOR\n\n\n"
