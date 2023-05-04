91edae1763fb8b13d7b96051df057ce80f2b2810
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-05-04 13:41:12 -0300

    perfecting error management

--- srcs/mandatory/error_management.c
+++ srcs/mandatory/error_management.c
@@ -3,35 +3,73 @@
 
 #include "averager.h"
 
-static void empty_string(char cmd[], FILE *fp, char *output) {
-    sprintf(cmd, "./push_swap %s", "");
+static void exec_test_process(char *cmd, FILE *fp, char *output, char *test) {
+    size_t bytes_read = fread(output, sizeof(char), 8000, fp);
+    if (!strncmp(test, "empty_string", 5) || !strncmp(test, "no_args", 5)) {
+        if (bytes_read)
+            printf(CYAN "%s----------- " HRED "Error\n" DEF_COLOR, test);
+        else
+            printf(CYAN "%s----------- " GREEN "OK\n" DEF_COLOR, test);
+    } else {
+        if (!strcmp(output, "Error\n"))
+            printf(CYAN "%s----------- " GREEN "OK\n" DEF_COLOR, test);
+        else
+            printf(CYAN "%s----------- " HRED "Error\n" DEF_COLOR, test);
+    }
+}
+
+static void empty_string(char *cmd, FILE *fp, char *output) {
+    sprintf(cmd, "./push_swap %s 2>&1", "");
     fp = popen(cmd, "r");
-    output = fgets(cmd, 8000, fp);
-    printf("%s \"\"\n", cmd);
-    if (output != NULL)
-        printf(HRED "Error\n" DEF_COLOR);
-    else
-        printf(CYAN "empty arg: " GREEN "OK\n" DEF_COLOR);
+    exec_test_process(cmd, fp, output, "empty_string ----");
     pclose(fp);
 }
 
-static void no_args(char cmd[], FILE *fp, char *output) {
-	sprintf(cmd, "./push_swap");
-	fp = popen(cmd, "r");
-	output = fgets(cmd, 8000, fp);
-	printf("%s\n", cmd);
-	if (output != NULL)
-		printf(HRED "Error\n" DEF_COLOR);
-	else
-		printf(CYAN "  no args: " GREEN "OK\n" DEF_COLOR);
+static void no_args(char *cmd, FILE *fp, char *output) {
+    sprintf(cmd, "./push_swap 2>&1");
+    fp = popen(cmd, "r");
+    exec_test_process(cmd, fp, output, "no_args ---------");
     pclose(fp);
+}
+
+static void duplicate_arg(char *cmd, FILE *fp, char *output) {
+    sprintf(cmd, "./push_swap %d %d %d 2>&1", 1, 2, 1);
+    fp = popen(cmd, "r");
+	exec_test_process(cmd, fp, output, "duplicate_arg ---");
+    pclose(fp);
+}
+
+static void duplicate_sorted(char *cmd, FILE *fp, char *output) {
+    sprintf(cmd,
+            "./push_swap %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d "
+            "%d 2>&1",
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 23, 24, 25,
+            26, 27, 28, 29, 29);
+    fp = popen(cmd, "r");
+	exec_test_process(cmd, fp, output, "duplicate_sorted ");
+    pclose(fp);
+}
+
+static void non_numeric(char *cmd, FILE *fp, char *output) {
+    sprintf(cmd, "./push_swap %d %d %d %s 2>&1", 3, 2, 1, "9a");
+	fp = popen(cmd, "r");
+	exec_test_process(cmd, fp, output, "non_numeric -----");
+	pclose(fp);
 }
 
 int main(void) {
-    char cmd[8000];
+    char *cmd;
     FILE *fp;
     char *output;
 
-	empty_string(cmd, fp, output);
-	no_args(cmd, fp, output);
+    cmd = calloc(sizeof(char), 8000);
+    output = calloc(sizeof(char), 8000);
+    empty_string(cmd, fp, output);
+    no_args(cmd, fp, output);
+    duplicate_arg(cmd, fp, output);
+    duplicate_sorted(cmd, fp, output);
+	non_numeric(cmd, fp, output);
+    free(cmd);
+    free(output);
+    printf(WHITE "\nERROR MANAGEMENT TESTS: " GREEN "OK\n\n" DEF_COLOR);
 }
