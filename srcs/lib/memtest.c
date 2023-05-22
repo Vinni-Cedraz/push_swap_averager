7148c880e84c24be63008a1ff65b526b782a5479
Vinni-Cedraz <Vinni-Cedraz@gmail.com>
2023-05-22 13:21:58 -0300

    basic_test.c is done, refactored down to ~80 lines
    It had 2400+ lines of code initially and used to take forever to compile
    
    	modified:   Makefile
    	modified:   srcs/lib/Makefile
    	modified:   srcs/lib/memtest.c
    	modified:   srcs/mandatory/basic_test.c

--- srcs/lib/memtest.c
+++ srcs/lib/memtest.c
@@ -1,11 +1,34 @@
 #include "../include/averager.h"
 
-void execute_memtest(int **table, int size, int i, char buffer[],
+void execute_memtest(int **table, int size, int table_index, char buffer[],
                      char cmd[], t_build_cmdstr build_cmdstr) {
-	build_cmdstr(cmd, i, table);
-    FILE *output = popen(cmd, "r");
-    fgets(buffer, 100, output);
-    log_cmd_and_output(table, size, i, buffer);
-    fprintf_ok_ko(buffer, output, NULL);
-    pclose(output);
+    FILE *output;
+
+    if (table_index != 1) {
+        build_cmdstr(cmd, table_index, table);
+        output = popen(cmd, "r");
+        fgets(buffer, 100, output);
+        log_cmd_and_output(table, size, table_index, buffer);
+        fprintf_ok_ko(buffer, output, NULL);
+        pclose(output);
+    } else if (build_cmdstr == build_memtest3_string) {
+        while (table[table_index]) {
+            build_cmdstr(cmd, table_index, table);
+            output = popen(cmd, "r");
+            fgets(buffer, 100, output);
+            log_cmd_and_output(table, 3, table_index, buffer);
+            fprintf_ok_ko(buffer, output, NULL);
+            pclose(output);
+            table_index++;
+        }
+    } else if (build_cmdstr == build_test3_string) {
+        while (table[table_index]) {
+            build_cmdstr(cmd, table_index, table);
+            output = popen(cmd, "r");
+            fgets(buffer, 100, output);
+            log_cmd_and_output_3(table, 3, table_index, buffer);
+            pclose(output);
+            table_index++;
+        }
+    }
 }
