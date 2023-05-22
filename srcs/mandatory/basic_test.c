517cdf386ad3694fde130492a867f3896d60d8ad
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-05-22 13:21:58 -0300

    basic_test.c is done, refactored down to ~80 lines
    It had 2400+ lines of code initially and used to take forever to compile
    
    	modified:   Makefile
    	modified:   srcs/lib/Makefile
    	modified:   srcs/lib/memtest.c
    	modified:   srcs/mandatory/basic_test.c

--- srcs/mandatory/basic_test.c
+++ srcs/mandatory/basic_test.c
@@ -1,109 +1,82 @@
 #include "../include/averager.h"
 
-static void execute_memtests(void *args_void) {
-  t_args *args = (t_args *)args_void;
-  int **table = args->table;
-  char buffer[100];
-  char cmd[10000];
-  FILE *output;
-  int i = 1;
-
-  while (table[i]) {
-    build_memtest3_string(cmd, i, table);
-    output = popen(cmd, "r");
-    fgets(buffer, 100, output);
-	log_cmd_and_output(table, 3, i, buffer);
-	fprintf_ok_ko(buffer, output, NULL);
-    pclose(output);
-    i++;
-  }
-
-  i = 1;
-  while (table[i]) {
-    build_test3_string(cmd, i, table);
-    output = popen(cmd, "r");
-    fgets(buffer, 100, output);
-	log_cmd_and_output_3(table, 3, i, buffer);
-    pclose(output);
-    i++;
-  }
-
-   dprintf(1, WHITE "\nMEMORY AND SORTING TESTS\n" DEF_COLOR CYAN
-                    "	with valgrind -q \n\n\n" DEF_COLOR);
-  i += 2;
-  int size = 5;
-
-  execute_memtest(table, size, i, buffer, cmd, build_memtest5_string);
-  i = 11;
-  size = 10;
-
-  build_memtest10_string(cmd, i, table);
-  output = popen(cmd, "r");
-  fgets(buffer, 100, output);
-  log_cmd_and_output(table, size, i, buffer);
-  fprintf_ok_ko(buffer, output, NULL);
-  pclose(output);
-
-  i = 15;
-  size = 15;
-
-  build_memtest15_string(cmd, i, table);
-  output = popen(cmd, "r");
-  fgets(buffer, 100, output);
-  log_cmd_and_output(table, size, i, buffer);
-  fprintf_ok_ko(buffer, output, NULL);
-  pclose(output);
-
-  i = 19;
-  size = 20;
-
-  build_memtest20_string(cmd, i, table);
-  output = popen(cmd, "r");
-  fgets(buffer, 100, output);
-  log_cmd_and_output(table, size, i, buffer);
-  fprintf_ok_ko(buffer, output, NULL);
-  pclose(output);
-
-  i = 27;
-  size = 100;
-
-  build_memtest100_string(cmd, i, table);
-  output = popen(cmd, "r");
-  fgets(buffer, 100, output);
-  log_cmd_and_output(table, size, i, buffer);
-  fprintf_ok_ko(buffer, output, NULL);
-  pclose(output);
-
-  i = 31;
-  size = 500;
-
-  build_memtest500_string(cmd, i, table);
-  output = popen(cmd, "r");
-  fgets(buffer, 100, output);
-  log_cmd_and_output(table, size, i, buffer);
-  fprintf_ok_ko(buffer, output, NULL);
-  pclose(output);
-}
+static void execute_memtests(void *args_void);
+static void log_memtests_header(void);
+static void log_exaustive_test20_header(void);
+static void log_memtests_footer(void);
+static void log_exaustive_test20_footer(void);
+static int size(void);
 
 int main(void) {
-  t_args *args = malloc(sizeof(t_args));
-  args->table = init_table();
-  dprintf(1, HRED "\n\nMAKE SURE YOU COMPILED EVERYTHING WITH THE -O3 "
-                  "FLAG\n\n\n" DEF_COLOR);
-  sleep(3);
-  dprintf(1, WHITE "BASIC TESTS\n" DEF_COLOR);
-  dprintf(1, CYAN "	with valgrind -q\n" DEF_COLOR);
-  execute_memtests(args);
-  dprintf(1, CYAN "\n\n\nIf you didnt see any valgrind messages it means no "
-                  "memory leaks were found in your program\n\n\n\n" DEF_COLOR);
-  dprintf(1,
-          HCYAN "Now, we will check sorting correctness with several different "
-                "permutations...\n" DEF_COLOR);
-  exaustive_test20(args);
-  ft_free_arr_size((void **)args->table, 1000);
-  free(args);
-  dprintf(
-      1, GREEN
-      "Sorting checks are done ->	" CYAN
-      "No (FAILED) messages in the next lines will mean its OK\n\n" DEF_COLOR);
+    t_args *args;
+
+    args = malloc(sizeof(t_args));
+    args->table = init_table();
+    log_memtests_header();
+    execute_memtests(args);
+    log_memtests_footer();
+    log_exaustive_test20_header();
+    exaustive_test20(args);
+    log_exaustive_test20_footer();
+    ft_free_arr_size((void **)args->table, 1000);
+    free(args);
+}
+
+static void execute_memtests(void *args_void) {
+    t_args *args = (t_args *)args_void;
+    int **table = args->table;
+    char buffer[100];
+    char cmd[10000];
+    FILE *output;
+
+    dprintf(1, WHITE "\nMEMORY AND SORTING TESTS\n" DEF_COLOR CYAN
+                     "	with valgrind -q \n\n\n" DEF_COLOR);
+
+    execute_memtest(table, size(), 01, buffer, cmd, build_memtest3_string);
+    execute_memtest(table, size(), 01, buffer, cmd, build_test3_string);
+    execute_memtest(table, size(), 07, buffer, cmd, build_memtest5_string);
+    execute_memtest(table, size(), 11, buffer, cmd, build_memtest10_string);
+    execute_memtest(table, size(), 15, buffer, cmd, build_memtest15_string);
+    execute_memtest(table, size(), 19, buffer, cmd, build_memtest20_string);
+    execute_memtest(table, size(), 27, buffer, cmd, build_memtest100_string);
+    execute_memtest(table, size(), 31, buffer, cmd, build_memtest500_string);
+}
+
+static int size(void) {
+    static short call_counter;
+    call_counter++;
+    if (call_counter == 1) return 3;
+    if (call_counter == 2) return 3;
+    if (call_counter == 3) return 5;
+    if (call_counter == 4) return 10;
+    if (call_counter == 5) return 15;
+    if (call_counter == 6) return 20;
+    if (call_counter == 7) return 100;
+    if (call_counter == 8) return 500;
+}
+
+static void log_memtests_header(void) {
+    dprintf(1, HRED
+            "\n\nMAKE SURE YOU COMPILED EVERYTHING WITH THE -O3 "
+            "FLAG\n\n\n" DEF_COLOR);
+    sleep(3);
+    dprintf(1, WHITE "BASIC TESTS\n" DEF_COLOR);
+}
+
+static void log_memtests_footer(void) {
+    dprintf(1, CYAN
+            "\n\n\nIf you didnt see any valgrind messages it means no "
+            "memory leaks were found in your program\n\n\n\n" DEF_COLOR);
+}
+
+static void log_exaustive_test20_header(void) {
+    dprintf(1, HCYAN
+            "Now, we will check sorting correctness with several different "
+            "permutations...\n" DEF_COLOR);
+}
+
+static void log_exaustive_test20_footer(void) {
+    dprintf(1, GREEN "Sorting checks are done ->	" CYAN
+                     "No (FAILED) messages in the next lines will mean its "
+                     "OK\n\n" DEF_COLOR);
 }
