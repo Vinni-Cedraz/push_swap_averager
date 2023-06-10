25edb483506451fa0f9449fa9225cedc64296ce9
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-06-10 12:09:17 -0300

    started refactoring identity_test.c to behave just like error_management.c

--- srcs/mandatory/identity_test.c
+++ srcs/mandatory/identity_test.c
@@ -4,10 +4,9 @@ static void get_test_process_output(FILE *fp, char *output, char *test) {
     size_t bytes_read = fread(output, sizeof(char), 8000, fp);
     if (bytes_read) {
         printf(CYAN "%s------- " HRED "KO" DEF_COLOR, test);
-		trim_linebreak(output);
-        printf(RED
-               "	Expected the string \"Error\\n\" on the stderr (fd "
-               "2)");
+        trim_linebreak(output);
+        printf(RED "	Expected the string \"Error\\n\" on the stderr (fd "
+                   "2)");
         printf(BLUE "	Got" DEF_COLOR " \"" DEF_COLOR " \%s\"" BLUE
                     " instead" DEF_COLOR,
                output);
@@ -15,42 +14,43 @@ static void get_test_process_output(FILE *fp, char *output, char *test) {
         printf(CYAN "%s------- " GREEN "OK" DEF_COLOR, test);
 }
 
-static void single_element(char *cmd, FILE *fp, char *output) {
-    sprintf(cmd, "valgrind -q ./push_swap %d 2>&1", 42);
-    fp = popen(cmd, "r");
+static void single_element(char *cmd, char *output) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all -q ./push_swap %d 2>&1",
+            42);
+    FILE *fp = popen(cmd, "r");
     get_test_process_output(fp, output, "<sorted> single_element -");
     printf("	<--- %s\n", cmd);
     pclose(fp);
 }
 
-static void four_elements(char *cmd, FILE *fp, char *output) {
-    sprintf(cmd, "valgrind -q ./push_swap %d %d %d %d 2>&1", 0, 1, 2, 3);
-    fp = popen(cmd, "r");
+static void four_elements(char *cmd, char *output) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all -q ./push_swap %d %d %d "
+            "%d 2>&1",
+            0, 1, 2, 3);
+    FILE *fp = popen(cmd, "r");
     get_test_process_output(fp, output, "<sorted> four_elements  -");
     printf("	<--- %s\n", cmd);
     pclose(fp);
 }
 
-static void nine_elements(char *cmd, FILE *fp, char *output) {
-    sprintf(cmd, "valgrind -q ./push_swap %d %d %d %d %d %d %d %d %d 2>&1", 0,
-            1, 2, 3, 4, 5, 6, 7, 8);
-    fp = popen(cmd, "r");
+static void nine_elements(char *cmd, char *output) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all -q ./push_swap %d %d %d "
+            "%d %d %d %d %d %d 2>&1",
+            0, 1, 2, 3, 4, 5, 6, 7, 8);
+    FILE *fp = popen(cmd, "r");
     get_test_process_output(fp, output, "<sorted> nine_elements  -");
     printf("	<--- %s\n", cmd);
     pclose(fp);
 }
 
 int main(void) {
-    char *cmd;
-    FILE *fp;
-    char *output;
+    char cmd[500];
+    char output[500];
 
-    fp = NULL;
-    cmd = calloc(sizeof(char), 8000);
-    output = calloc(sizeof(char), 8000);
-    single_element(cmd, fp, output);
-    four_elements(cmd, fp, output);
-    nine_elements(cmd, fp, output);
-    free(cmd);
-    free(output);
+    single_element(cmd, output);
+    four_elements(cmd, output);
+    nine_elements(cmd, output);
 }
