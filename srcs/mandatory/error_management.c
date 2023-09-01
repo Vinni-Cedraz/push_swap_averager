51c69d2d8eaf740dd8660ddebec9a5d036a1ebdd
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-09-01 20:15:11 -0300

    corrected the num-numeric argument error_management
    included more types of non-numeric arguments and also fixed what is
    expected from an empty string (which is the same as is expected from a
    non-numeric argument, instead of what is expected from no argument at
    all)
    
    modified:   Makefile
    modified:   srcs/mandatory/error_management.c

--- srcs/mandatory/error_management.c
+++ srcs/mandatory/error_management.c
@@ -28,7 +28,13 @@ static void whitespaced_action(char *cmd);
 static void duplicate_arg(char *cmd);
 static void duplicate_sorted(char *cmd);
 static void max_int_overf(char *cmd);
-static void non_numeric(char *cmd);
+static void non_numeric1(char *cmd);
+static void non_numeric2(char *cmd);
+static void non_numeric3(char *cmd);
+static void non_numeric4(char *cmd);
+static void non_numeric5(char *cmd);
+static void non_numeric6(char *cmd);
+static void non_numeric_empty_string(char *cmd);
 static void open_process_and_exec_cmd_there(FILE **fp, char *cmd, bool close);
 static void trim_linebreak(char *str);
 
@@ -36,46 +42,101 @@ int main(void) {
     char cmd[500];
     char output[500];
 
-    printf(CYAN "no_args		 ------  " DEF_COLOR);
+    printf(CYAN "no_args        ------     " DEF_COLOR);
     no_args(cmd);
-    printf(CYAN "empty string	 ------  " DEF_COLOR);
-    empty_string(cmd);
-    printf(CYAN "non_numeric	 ------  " DEF_COLOR);
-    non_numeric(cmd);
-    printf(CYAN "max_int_overf	 ------  " DEF_COLOR);
+    printf(CYAN "non_numeric  1 ------     " DEF_COLOR);
+    non_numeric1(cmd);
+    printf(CYAN "non_numeric  2 ------     " DEF_COLOR);
+    non_numeric2(cmd);
+    printf(CYAN "non_numeric  3 ------     " DEF_COLOR);
+    non_numeric3(cmd);
+    printf(CYAN "non_numeric  4 ------     " DEF_COLOR);
+    non_numeric4(cmd);
+    printf(CYAN "non_numeric  5 ------     " DEF_COLOR);
+    non_numeric5(cmd);
+    printf(CYAN "non_numeric  6 ------     " DEF_COLOR);
+    non_numeric6(cmd);
+    printf(CYAN "non_numeric  empty_string " DEF_COLOR);
+    non_numeric_empty_string(cmd);
+    printf(CYAN "max_int_overf  -------    " DEF_COLOR);
     max_int_overf(cmd);
-    printf(CYAN "duplicate_sorted ------  " DEF_COLOR);
+    printf(CYAN "duplicate_sorted -----    " DEF_COLOR);
     duplicate_sorted(cmd);
 }
 
 static void no_args(char *cmd) {
-    sprintf(cmd, "valgrind --leak-check=full --show-leak-kinds=all "
+    sprintf(cmd, "valgrind --leak-check=full --show-leak-kinds=all"
                  " -q ./push_swap 2>&1");
-    get_test_output(cmd, 1);
+    get_test_output(cmd, EMPTY_EXPECTED);
     system("rm -f error.log");
 }
 
-static void empty_string(char *cmd) {
+static void non_numeric1(char *cmd) {
     sprintf(cmd,
-            "valgrind --leak-check=full --show-leak-kinds=all "
-            " -q ./push_swap \"%s\" 2>&1",
-            "");
-    get_test_output(cmd, 1);
-    system("rm -f error.log");
-}
-
-static void non_numeric(char *cmd) {
-    sprintf(cmd,
-            "valgrind --leak-check=full --show-leak-kinds=all "
+            "valgrind --leak-check=full --show-leak-kinds=all"
             " -q ./push_swap %d %d %d %s 2>error.log",
             3, 2, 1, "9a");
-    get_test_output(cmd, 0);
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
+    system("rm -f error.log");
+}
+
+static void non_numeric2(char *cmd) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all"
+            " -q ./push_swap %d %d %d %s 2>error.log",
+            3, 2, 1, "a");
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
+    system("rm -f error.log");
+}
+
+static void non_numeric3(char *cmd) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all"
+            " -q ./push_swap %d %d %d %s 2>error.log",
+            3, 2, 1, "12 a 2");
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
+    system("rm -f error.log");
+}
+
+static void non_numeric4(char *cmd) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all"
+            " -q ./push_swap %d %d %d %s 2>error.log",
+            3, 2, 1, "12 - 2");
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
+    system("rm -f error.log");
+}
+
+static void non_numeric5(char *cmd) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all"
+            " -q ./push_swap %d %d %d %s 2>error.log",
+            3, 2, 1, "12 --2");
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
+    system("rm -f error.log");
+}
+
+static void non_numeric6(char *cmd) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all"
+            " -q ./push_swap %d %d %d %s 2>error.log",
+            3, 2, 1, "12 ++2");
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
+    system("rm -f error.log");
+}
+
+static void non_numeric_empty_string(char *cmd) {
+    sprintf(cmd,
+            "valgrind --leak-check=full --show-leak-kinds=all -q ./push_swap"
+            " \"%s\" 2>error.log",
+            "");
+    get_test_output(cmd, EMPTY_NOT_EXPECTED);
     system("rm -f error.log");
 }
 
 static void max_int_overf(char *cmd) {
     sprintf(cmd,
-            "valgrind --leak-check=full --show-leak-kinds=all "
+            "valgrind --leak-check=full --show-leak-kinds=all"
             " -q ./push_swap %d %d %d %lu 2>error.log",
             35, 24, 21, 21474836498);
     get_test_output(cmd, 0);
@@ -84,7 +145,7 @@ static void max_int_overf(char *cmd) {
 
 static void duplicate_sorted(char *cmd) {
     sprintf(cmd,
-            "valgrind --leak-check=full --show-leak-kinds=all "
+            "valgrind --leak-check=full --show-leak-kinds=all"
             " -q ./push_swap %d %d %d %d %d %d 2>error.log",
             10, 11, 12, 13, 14, 14);
     get_test_output(cmd, 0);
@@ -93,7 +154,7 @@ static void duplicate_sorted(char *cmd) {
 
 static void duplicate_arg(char *cmd) {
     sprintf(cmd,
-            "valgrind --leak-check=full --show-leak-kinds=all "
+            "valgrind --leak-check=full --show-leak-kinds=all"
             " -q ./push_swap %d %d %d 2>error.log",
             1, 2, 1);
     get_test_output(cmd, 0);
@@ -135,16 +196,25 @@ static void analyse_cmd_output(char line[500], FILE *fp, char cmd[500]) {
 static void analyse_error_log_file(char line[500], char cmd[500]) {
     ssize_t read;
     FILE *error_log = fopen("error.log", "r");
+    if (error_log == NULL)
+        perror("Error opening file");
+    fseek(error_log, 0, SEEK_END);
+    if (ftell(error_log) == 0) {
+        log_error(EMPTY_NOT_EXPECTED, line, cmd);
+        return;
+    }
+    rewind(error_log);
     if ((read = fread(line, sizeof(char), 500, error_log)) != -1) {
         if (!strcmp(line, "Error\n")) {
             printf(GREEN "OK" DEF_COLOR " <---- ");
             printf("%s\n", cmd);
-        } else if (NULL != strstr(line, "egmentation"))
-            printf(RED "KO" DEF_COLOR "	<---- SEGFAULT\n"), exit(1);
-        else if (NULL != strstr(line, "memcheck"))
+        } else if (NULL != strstr(line, "egmentation")) {
+            printf(RED "KO" DEF_COLOR "	<---- SEGFAULT\n");
+            exit(1);
+        } else if (NULL != strstr(line, "memcheck")) {
             printf(RED "KO" DEF_COLOR "	<---- MEMORY LEAK on %s\n", cmd);
-    } else
-        log_error(EMPTY_NOT_EXPECTED, line, cmd);
+        }
+    }
     fclose(error_log);
 }
 
@@ -171,10 +241,8 @@ static void log_error(bool empty_expected, char *out_str, char *cmd) {
     } else if (!empty_expected) {
         printf(cmd);
         printf(HRED " KO" DEF_COLOR);
-        printf(RED "	Expected the string \"Error\\n\" on the stderr (fd "
-                   "2)");
-        printf(BLUE "	Got" DEF_COLOR " \"" DEF_COLOR " \%s\"" BLUE
-                    " instead\n" DEF_COLOR,
+        printf(RED "	Expected the string \"Error\\n\" on the stderr (fd 2)");
+        printf(BLUE "	Got " DEF_COLOR "\"\"" BLUE " instead\n" DEF_COLOR,
                out_str);
     }
 }
